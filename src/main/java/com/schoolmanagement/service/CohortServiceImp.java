98c4efece798d79adad5f74beb324032208a369d
scepter <chukwunenyeebuka@gmail.com>
2024-06-19 08:18:34 +0100

    Update

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -114,7 +114,6 @@ public class CohortServiceImp implements CohortService {
     }
 
 
-
     public String uploadProfileImage(MultipartFile file, Long cohortId) {
       //  cohortRepository.findById(cohortId).get().setCohortAvatar(
          return   cloudService.uploadFile(file);
