404913cf2ea2d374bbd462f23e448aa9c1d67f8d
scepter <chukwunenyeebuka@gmail.com>
2024-06-21 09:48:46 +0100

    Update

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -39,7 +39,6 @@ public class CohortServiceImp implements CohortService {
         cohortResponse.setStartDate(formattedStartDate);
         LocalDate endDate = LocalDate.parse(cohort.getEndDate());
 
-
         String formattedEndDate = endDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         cohortResponse.setEndDate(formattedEndDate);
         cohortResponse.setEnumProgram((cohort.getEnumProgram()));
