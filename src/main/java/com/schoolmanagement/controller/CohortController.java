e6e52c8729cfbe6281c825e1bbd238b9a22e4a55
scepter <chukwunenyeebuka@gmail.com>
2024-07-08 09:56:19 +0100

    Update

--- src/main/java/com/schoolmanagement/controller/CohortController.java
+++ src/main/java/com/schoolmanagement/controller/CohortController.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 
 
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
