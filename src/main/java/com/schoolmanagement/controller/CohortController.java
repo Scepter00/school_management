26eb99f09299086df635499691bc19d180af610a
scepter <chukwunenyeebuka@gmail.com>
2024-07-11 09:42:45 +0100

    Update

--- src/main/java/com/schoolmanagement/controller/CohortController.java
+++ src/main/java/com/schoolmanagement/controller/CohortController.java
@@ -14,6 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
+
+
+
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/school")
