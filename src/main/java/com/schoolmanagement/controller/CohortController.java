b7160a24bc12eb185a23d89631d855404a99128b
scepter <chukwunenyeebuka@gmail.com>
2024-07-16 10:41:22 +0100

    Update.

--- src/main/java/com/schoolmanagement/controller/CohortController.java
+++ src/main/java/com/schoolmanagement/controller/CohortController.java
@@ -33,7 +33,6 @@ public class CohortController {
 
 
 
-
     @PostMapping("/createCohort")
     public ResponseEntity<CohortResponse> createCohort(@RequestBody CohortRequest cohortRequest) {
         CohortResponse createdResponse = cohortServiceImp.createCohort(cohortRequest);
