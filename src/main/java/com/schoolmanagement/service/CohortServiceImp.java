3d7a9cc930bded858c84f0557177bc092be3e8c5
scepter <chukwunenyeebuka@gmail.com>
2024-07-30 10:14:20 +0100

    Update.

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -33,7 +33,6 @@ public class CohortServiceImp implements CohortService {
 
 
 
-
     private CohortResponse createCohortResponse(Cohort cohort) {
         EnumProgram enumProgram;
         CohortResponse cohortResponse = new CohortResponse();
