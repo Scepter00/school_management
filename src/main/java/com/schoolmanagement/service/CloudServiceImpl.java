4afdb37b4a4d5022cec013d685e9ad87925bead0
scepter <chukwunenyeebuka@gmail.com>
2024-11-15 17:34:27 +0100

    Update.

--- src/main/java/com/schoolmanagement/service/CloudServiceImpl.java
+++ src/main/java/com/schoolmanagement/service/CloudServiceImpl.java
@@ -17,7 +17,6 @@ public class CloudServiceImpl implements CloudService {
     @Override
     public String uploadFile(MultipartFile file) {
         try {
-
             final Map<?, ?> response = cloudinary.uploader()
                     .upload(
                             file.getBytes(),
