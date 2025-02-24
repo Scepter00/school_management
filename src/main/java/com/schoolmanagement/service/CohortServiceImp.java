6e9444a081a2e6c661c3120f4c317ef59b35a47c
scepter <chukwunenyeebuka@gmail.com>
2025-02-25 03:19:44 +1400

    Update.

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -51,7 +51,6 @@ public class CohortServiceImp implements CohortService {
         if (cohortRepository.findByCohortName(createCohortRequest.getCohortName()) != null) {
             throw new RuntimeException("Cohort already exists");
         }
-
         Cohort cohort = new Cohort();
 
         cohort.setCohortName(createCohortRequest.getCohortName());
