329ac754139bbf5227fabad5b84cf7c2b34fc7ff
scepter <chukwunenyeebuka@gmail.com>
2025-05-28 01:13:10 +1400

    Update.

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -46,6 +46,11 @@ public class CohortServiceImp implements CohortService {
         return cohortResponse;
     }
 
+
+
+
+
+
     @Override
     public CohortResponse createCohort(CohortRequest createCohortRequest) {
         if (cohortRepository.findByCohortName(createCohortRequest.getCohortName()) != null) {
