a4fee9443d72917d42dfa1568e21f455f8e240a8
scepter <chukwunenyeebuka@gmail.com>
2025-08-17 07:36:06 +0100

    Update.

--- src/main/java/com/schoolmanagement/service/CohortServiceImp.java
+++ src/main/java/com/schoolmanagement/service/CohortServiceImp.java
@@ -61,7 +61,6 @@ public class CohortServiceImp implements CohortService {
         cohort.setEndDate(endDate.toString());
         cohort.setEnumProgram(createCohortRequest.getEnumProgram());
 
-
         String program  = String.valueOf(createCohortRequest.getEnumProgram());
 
         List <String> programNames = programRepository.findAll()
