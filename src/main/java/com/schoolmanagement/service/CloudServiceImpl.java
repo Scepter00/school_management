79e65317dcbdffb8985dbba687648391124e3b88
scepter <chukwunenyeebuka@gmail.com>
2025-10-05 22:33:22 -1200

    Update.

--- src/main/java/com/schoolmanagement/service/CloudServiceImpl.java
+++ src/main/java/com/schoolmanagement/service/CloudServiceImpl.java
@@ -23,7 +23,6 @@ public class CloudServiceImpl implements CloudService {
                             file.getBytes(),
                             ObjectUtils.emptyMap()
                     );
-            
             return response.get("url").toString();
         } catch (IOException ex) {
             throw new IllegalArgumentException(ex.getMessage());
